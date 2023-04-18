130939cf94b715948f2614642eee7996b07ae461
Neto test <foxekaterina@yandex.ru>
2023-04-18 10:39:49 +0900

    Added allure

--- src/test/java/ru/netology/devilery/test/DeliveryTest.java
+++ src/test/java/ru/netology/devilery/test/DeliveryTest.java
@@ -1,6 +1,9 @@
 package ru.netology.devilery.test;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,6 +17,16 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DeliveryTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @BeforeAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
     void setup() {
